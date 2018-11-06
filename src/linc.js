e92debc951d697f7964c72619392dd878b501225
lixiaojun <lixiaojun629@163.com>
2018-11-06 17:31:24 +0800

    first commit

---
+++ src/linc.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+/**
+ * 检查变更的代码是否符合规范
+ */
+const path = require('path');
+const CLIEngine = require('eslint').CLIEngine;
+const listChangedFiles = require('./listChangedFiles');
+
+const ignoreMessage = 'File ignored';
+
+function runESLint() {
+    const options = { configFile: path.resolve(__dirname, '../.eslintrc.js') };
+    const cli = new CLIEngine(options);
+    let changeFiles = [...listChangedFiles()];
+    changeFiles = changeFiles.filter(item => !!item);
+    const report = cli.executeOnFiles(changeFiles);
+    const errorFiles = report.results.filter(item => {
+        return item.messages.length > 0 && item.messages[0].message.indexOf(ignoreMessage) !== 0;
+    });
+
+    const result = {
+        errorCount: report.errorCount
+    };
+
+    if (result.errorCount) {
+        console.log(`总错误次数：${result.errorCount}`);
+        errorFiles.forEach(item => {
+            console.log(`文件路径：${item.filePath}`);
+            item.messages.forEach(message => {
+                console.log(
+                    `错误行号：${message.line}, Message: ${message.message} RuleId:${
+                        message.ruleId
+                    }`
+                );
+            });
+        });
+        process.exit(1);
+    }
+}
+
+runESLint();
---
+++ src/readme.md
@@ -0,0 +1,15 @@
+### 简介
+ 代码风格检查执行模块
+### 安装
+```
+npm install @ucloud-fe/linc
+```
+
+### 检查已变更（git）js文件
+```
+npm run linc
+```
+### 检查项目中所有js文件
+```
+npm run format
+```
